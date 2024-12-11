d1f5f58757bee623ed27cc95fb72dd75f5af1575
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-12-11 20:31:39 +0100

    Using Hr component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Wrapper from "./components/wrapper/Wrapper";
 import IntroSection from "./components/introSection/IntroSection";
+import Hr from "./components/utils/Hr";
 
 function App() {
   return (
     <Wrapper>
       <IntroSection />
+      <Hr />
     </Wrapper>
   );
 }
