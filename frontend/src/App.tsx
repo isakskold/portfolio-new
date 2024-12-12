009e57e7deb968225d38c404019c9d130b76fda9
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-12-12 17:45:20 +0100

    Corrected import path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Wrapper from "./components/wrapper/Wrapper";
-import IntroSection from "./components/introSection/IntroSection";
+import IntroSection from "./components/sections/introSection/IntroSection";
 import Hr from "./components/utils/Hr";
 
 function App() {
