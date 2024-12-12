490bc2073d0c9e2ce34b82807ec4c4d9be1aba20
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-12-12 17:48:58 +0100

    Render project section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import Wrapper from "./components/wrapper/Wrapper";
-import IntroSection from "./components/sections/introSection/IntroSection";
 import Hr from "./components/utils/Hr";
+import IntroSection from "./components/sections/introSection/IntroSection";
+import ProjectSection from "./components/sections/projectSection/ProjectSection";
 
 function App() {
   return (
     <Wrapper>
       <IntroSection />
       <Hr />
+      <ProjectSection />
     </Wrapper>
   );
 }
