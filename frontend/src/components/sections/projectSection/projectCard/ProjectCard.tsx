dfaac0b784b5e0c5db741bec4120d42b2fdb2beb
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-12-14 22:10:41 +0100

    Hover effect on card header

--- frontend/src/components/sections/projectSection/projectCard/ProjectCard.tsx
+++ frontend/src/components/sections/projectSection/projectCard/ProjectCard.tsx
@@ -12,13 +12,18 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
     <div className="w-52 flex-shrink-0 rounded-2xl bg-greyCustom shadow-[0_-2px_12px_rgba(255,255,255,0.4),0_3px_6px_rgba(255,255,255,0.3)] sm:w-60 md:w-72">
       {/* Card "Header" */}
       <div
-        className="flex cursor-pointer items-center justify-between p-3"
+        className="transition-padding group flex cursor-pointer items-center justify-between p-3 duration-300 hover:py-[6px]"
+        title="View Details"
         onClick={() => setActiveProject(project)}
       >
         <h3 className="text-base font-bold sm:text-lg md:text-xl">
           {project.title}
         </h3>
-        <img src="/eyeIcon.svg" alt="Eye Icon" className="h-auto w-8" />
+        <img
+          src="/eyeIcon.svg"
+          alt="Eye Icon"
+          className="transition-width h-auto w-8 duration-300 group-hover:w-11"
+        />
       </div>
 
       {/* Display the first image in the project object */}
