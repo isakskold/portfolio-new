0ea399c11442e94c01ed538d4017e32f3835b22f
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2025-05-10 19:24:46 +0200

    google analytics scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import GoogleAnalytics from "./components/GoogleAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <GoogleAnalytics />
         <main className="">{children}</main>
       </body>
     </html>
